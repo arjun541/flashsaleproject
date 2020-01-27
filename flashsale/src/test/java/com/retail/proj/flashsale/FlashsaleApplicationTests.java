2cd2a2d1ab57cc7d53814b938d9e67ca42aa6c98
mallikarjunar <bmkr.541@gmail.com>
2020-01-27 12:48:45 +0530

    added the spring project

---
+++ flashsale/src/test/java/com/retail/proj/flashsale/FlashsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.proj.flashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
